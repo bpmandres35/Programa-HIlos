4a4f0b641186aa73281aafd8ed0548480c4153ed
bpmandres35 <104940769+bpmandres35@users.noreply.github.com>
2022-12-08 13:36:52 -0500

    Add files via upload

---
+++ Limpiar.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hilossincronizados;
+
+/**
+ *
+ * @author Elkin Pinzon
+ */
+public class Limpiar extends Thread{
+    
+    @Override
+    public void run() {
+        
+    }
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
